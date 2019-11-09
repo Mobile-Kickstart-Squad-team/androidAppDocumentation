03a4399d88f4d22d146a33a0e5cb99481ee6fcbb
yingkin <ying@yingkin_server_raid1.attlocal.net>
2019-11-09 11:37:25 +0000

    Updates

---
+++ precache-manifest.699150859e5effb22acabaf23c268f40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf77baa950519731243fd1337e99544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddbde0a8a478e5c9c399",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b678bdb0c3dd954b571",
+    "url": "/static/js/2.3bb83ea8.chunk.js"
+  },
+  {
+    "revision": "ddbde0a8a478e5c9c399",
+    "url": "/static/js/main.bb166173.chunk.js"
+  },
+  {
+    "revision": "9683e9b515bf6a8682dc",
+    "url": "/static/js/runtime-main.bbc28118.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
