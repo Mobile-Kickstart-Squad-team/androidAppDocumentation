8d29075eb929895a827d6d5a766d2d56310c44cc
yingkin <ying@yingkin_server_raid1.attlocal.net>
2019-11-23 08:02:50 +0000

    fix second blog link third time

--- src/pages/blog-sidebar.jsx
+++ src/pages/blog-sidebar.jsx
@@ -16,7 +16,7 @@ const BlogSideBar = () => {
                     <li><a href="#"><Link to="/blog/posting-ios-app-story">
                         The journey of posting an IOS app for review</Link></a>
                     </li>
-                    <li><a href="/blog/apache-spark">Practice Apache Spark sql and dataframes</a></li>
+                    <li><Link to="/blog/apache-spark">Practice Apache Spark sql and dataframes</Link></li>
                     <li><a href="#" class="nav-link disabled">&#9776; open</a></li>
                 </ul>
                 
